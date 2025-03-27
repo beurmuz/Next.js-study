245b840c6498e19dc222010308388d52e375eb04
beurmuz <fallinta2@gmail.com>
2025-03-28 01:59:59 +0900

    cinema 실습) 페이지별 내용 수정

--- onebite-cinema/src/pages/movie/[id].tsx
+++ onebite-cinema/src/pages/movie/[id].tsx
@@ -3,5 +3,5 @@ import { useRouter } from "next/router";
 export default function Page() {
   const router = useRouter();
   const { id } = router.query;
-  return <h2>{id}: 영화 상세페이지</h2>;
+  return <h2>{id} 영화 상세페이지</h2>;
 }
--- onebite-cinema/src/pages/search/index.tsx
+++ onebite-cinema/src/pages/search/index.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 export default function Page() {
   const router = useRouter();
   const { q } = router.query;
-  return <h2>{q} 영화 상세페이지</h2>;
+  return <h2>검색 결과: {q}</h2>;
 }
 
 Page.getLayout = (page: ReactNode) => {
