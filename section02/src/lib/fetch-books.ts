6d1b07861ab7a21d3c6112e90f1d8e9b0de8a10b
beurmuz <fallinta2@gmail.com>
2025-03-31 21:47:10 +0900

    section02) ssr 실습1 - 서치, 상세 페이지 데이터 페칭

--- section02/src/lib/fetch-books.ts
+++ section02/src/lib/fetch-books.ts
@@ -1,15 +1,19 @@
 import { BookData } from "@/types";
 
-export default async function fetchBooks(): Promise<BookData[]> {
-  const url = "http://localhost:12345/book";
+export default async function fetchBooks(q?: string): Promise<BookData[]> {
+  let url = "http://localhost:12345/book";
+
+  // 검색어가 전달되었다면, 검색 결과를 불러오는 api를 호출한다.
+  if (q) {
+    url += `/search?q=${q}`;
+  }
 
   try {
     const response = await fetch(url);
     if (!response.ok) throw new Error();
 
     return await response.json();
-  } 
-  catch (err) {
+  } catch (err) {
     console.error(err);
     return [];
   }
